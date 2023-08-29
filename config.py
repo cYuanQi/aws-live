87749e5804acdccc34e22cbcc27c074e33e4d111
cYuanQi <87699309+cYuanQi@users.noreply.github.com>
2023-08-29 16:31:07 +0800

    pwd

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-1.cqfzqqxsfxfy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "BAIT2373"
 customdb = "employee"
 custombucket = "chongyq-employee"
 customregion = "us-east-1"
